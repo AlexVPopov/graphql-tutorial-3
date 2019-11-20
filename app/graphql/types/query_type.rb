d73f7a532dc67821366621bc5435f911a493a94b
Alexander Popov <alex.val.popov@gmail.com>
2019-11-20 13:52:06 +0200

    Add a couple of more queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,20 @@ module Types
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
     field :all_links, [LinkType], null: false
+    field :first_link, LinkType, null: false
+    field :last_link, LinkType, null: false
 
     # this method is invoked, when `all_link` fields is being resolved
     def all_links
       Link.all
+    end
+
+    def first_link
+      Link.first
+    end
+
+    def last_link
+      Link.last
     end
   end
 end
