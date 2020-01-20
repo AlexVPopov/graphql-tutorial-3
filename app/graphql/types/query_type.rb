ecdff2155ed5c0d4f560a5d1411260dc9f96184e
Alexander Popov <alex.val.popov@gmail.com>
2020-01-20 17:53:24 +0200

    Use LinksSearch as a resolve function

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,14 +2,9 @@ module Types
   class QueryType < BaseObject
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, [LinkType], null: false
+    field :all_links, function: Resolvers::LinksSearch
     field :first_link, LinkType, null: false
     field :last_link, LinkType, null: false
-
-    # this method is invoked, when `all_link` fields is being resolved
-    def all_links
-      Link.all
-    end
 
     def first_link
       Link.first
